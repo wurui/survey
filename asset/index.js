dfbd4686364ed51176737e5cda417d07ee612476
wurui <wurui.wr@gmail.com>
2018-06-10 16:40:20 +0800

    fix fix

--- asset/index.js
+++ asset/index.js
@@ -2,13 +2,21 @@ define(['oxjs'],function(OXJS){
   return {
     init:function($mod){
     	var $f=$('form',$mod).on('submit',function(){
-    		$mod.OXPost({
-	    		'form-post':{
-	    			uid:OXJS.getUID(),
-	    			post:$f.serializeArray(),
-	    			time:Date.now()
-	    		}
-	    	})
+    		try{
+    			$mod.OXPost({
+		    		'form-post':{
+		    			uid:OXJS.getUID(),
+		    			post:$f.serializeArray(),
+		    			time:Date.now()
+		    		}
+		    	},function(){
+		    		OXJS.toast('提交成功！')
+		    	})
+    		}catch(e){
+
+    		}
+    		return false
+    		
 
     	});
     	
